83c9965eaa0f1bd14464f1f08d634857f285dc49
spencer.heim <spencer.heim@balconia.co.jp>
2022-08-18 15:23:36 +0900

    click events

--- pages/components/GameBoard/GameBoard.js
+++ pages/components/GameBoard/GameBoard.js
@@ -1,32 +1,44 @@
+import React, { useState } from "react";
 import styles from "./GameBoard.module.scss";
 import GameRow from "../GameRow/GameRow";
 import GameCell from "../GameCell/GameCell";
 
 function GameBoard(props) {
   const boardSize = 10;
+  const [getClickState, setClickState] = useState(0);
+  
+  const [getArray, setArray] = useState([]);
+  // 1 = left click
+  // 2 = right click
 
-  var rowArray = [];
-  var cellArray = [];
+  function handleMouseDown(e){
+    if (e.button == 2) {
+      setClickState(2);
+    } else{
+      setClickState(1);
+    }
+  }
 
+
+  var rowArray =[];
   return (
     <>
-      <div className={styles.gameBoard}>
+      <div className={styles.gameBoard} onMouseDown={handleMouseDown} onMouseUp={() => setClickState(0)}>
         {(() => {
-          let rows = [];
           for (let j = 1; j <= boardSize; j++) {
-            rows.push(
+            rowArray.push(
               <GameRow className={styles.gameRow} key={j}>
                 {(() => {
-                  let cells = [];
+                  let cellArray = [];
                   for (let i = 1; i <= boardSize; i++) {
-                    cells.push(<GameCell className={styles.gameCell} key={i}></GameCell>);
+                    cellArray.push(<GameCell className={styles.gameCell} key={j + '-' + i} clickState={getClickState} rowArray={rowArray} row={i} column={j}></GameCell>);
                   }
-                  return cells;
+                  return cellArray;
                 })()}
               </GameRow>
             );
           }
-          return rows;
+          return rowArray;
         })()}
       </div>
     </>
--- pages/components/GameCell/GameCell.js
+++ pages/components/GameCell/GameCell.js
@@ -1,13 +1,39 @@
+import React, { useState } from "react";
 import styles from "./GameCell.module.scss";
 
 function GameCell(props) {
-  
-  const { children } = props;
+  const { clickState, row, column } = props;
+  const [getClass, setClass] = useState(0);
+  const [isClicking, setClicking] = useState(false);
+  // 0 = empty
+  // 1 = filled
+  // 2 = X
 
-  return (
-      <div className={styles.gameCell}>
-      </div>
-  );
+
+  function handleMouseDown(e) {
+    e.preventDefault();
+    setClicking(true);
+    if (e.button == 2) {
+      getClass == 2 ? setClass(0) : setClass(2);
+    } else{
+      getClass == 1 ? setClass(0) : setClass(1);
+    }
+  }
+
+  function handleEnter(e) {
+    if (clickState == 1) {
+      getClass == 1 ? setClass(0) : setClass(1);
+    } else if (clickState == 2) {
+      getClass == 2 ? setClass(0) : setClass(2);
+    }
+  }
+
+  function handleMouseUp(e) {
+    e.preventDefault();
+    setClicking(false);
+  }
+
+  return <div className={`${styles.gameCell} ${getClass == 1 ? styles.filled : getClass == 2 ? styles.crossed : ""}`} onMouseDown={handleMouseDown} onMouseEnter={handleEnter} onMouseUp={handleMouseUp} onContextMenu={(e)=> e.preventDefault()}></div>;
 }
 
 export default GameCell;
