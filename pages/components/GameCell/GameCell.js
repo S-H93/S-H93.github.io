795e104bd2f098e200510909a64295d55a0df02e
spencer.heim <spencer.heim@balconia.co.jp>
2022-08-25 17:22:28 +0900

    Remove GameCell's class state

--- pages/components/GameCell/GameCell.js
+++ pages/components/GameCell/GameCell.js
@@ -3,27 +3,22 @@ import styles from "./GameCell.module.scss";
 
 function GameCell(props) {
   const { clickState, getBeforeTile, setBeforeTile, row, column, getArray, updateArray } = props;
-  const [getClass2, setClass2] = useState(0);
   // 0 = empty
   // 1 = filled
   // 2 = X
 
-  useEffect(() => {
-    setClass(getClass());
-  });
 
   function getClass(){
     return getArray[row][column];
   }
 
   function setClass(val){
-    setClass2(val);
     updateArray(row, column, val);
   }
 
   function handleMouseDown(e) {
     e.preventDefault();
-    setBeforeTile(getClass2);
+    setBeforeTile(getClass());
     if (e.button == 2) { // right click
       getClass() == 0 ? setClass(2) : setClass(0);
     } else{
@@ -48,7 +43,7 @@ function GameCell(props) {
     setBeforeTile(undefined);
   }
 
-  return <div className={`${styles.gameCell} ${getClass2 == 1 ? styles.filled : getClass2 == 2 ? styles.crossed : ""}`} onMouseDown={handleMouseDown} onMouseEnter={handleEnter} onMouseUp={handleMouseUp} onContextMenu={(e)=> e.preventDefault()}></div>;
+  return <div className={`${styles.gameCell} ${getClass() == 1 ? styles.filled : getClass() == 2 ? styles.crossed : ""}`} onMouseDown={handleMouseDown} onMouseEnter={handleEnter} onMouseUp={handleMouseUp} onContextMenu={(e)=> e.preventDefault()}></div>;
 }
 
 export default GameCell;
