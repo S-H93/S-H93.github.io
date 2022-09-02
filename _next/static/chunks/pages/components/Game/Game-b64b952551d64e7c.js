e091be4c6507d809bfed7e7daec27351da41b701
S-H93 <S-H93@users.noreply.github.com>
2022-09-02 09:08:37 +0000

    deploy: 3c015b03e2726a3964054fa688695b3bf5786211

---
+++ _next/static/chunks/pages/components/Game/Game-b64b952551d64e7c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[404],{1062:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Game/Game",function(){return u(8363)}])}},function(n){n.O(0,[774,363,888,179],(function(){return _=1062,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
