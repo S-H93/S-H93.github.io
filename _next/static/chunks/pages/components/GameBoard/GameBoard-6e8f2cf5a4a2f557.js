8af736c8404ce6784c8d582c2d217eede6aea300
S-H93 <S-H93@users.noreply.github.com>
2022-09-14 02:08:55 +0000

    deploy: cde60db80276b395ceecbf1d3eca624a7fbd1adf

---
+++ _next/static/chunks/pages/components/GameBoard/GameBoard-6e8f2cf5a4a2f557.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[450],{1479:function(n,_,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/GameBoard/GameBoard",function(){return o(2617)}])}},function(n){n.O(0,[774,617,888,179],(function(){return _=1479,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
