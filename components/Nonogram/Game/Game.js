ab9058c8058f02c5f9b1465d9ef806c9c7b687d5
spencer.heim <spencer.heim@balconia.co.jp>
2022-09-16 15:46:20 +0900

    Shake animation when incorrect

--- components/Nonogram/Game/Game.js
+++ components/Nonogram/Game/Game.js
@@ -7,14 +7,14 @@ import puzzles from "../data/puzzles10.json"; // JSON containing all the puzzles
 function Game(props) {
   const keys = Object.keys(puzzles); // keys of all the puzzles in the JSON file
   const [getPuzzleNum, setPuzzleNum] = useState(-1); // key of the current puzzle (-1 initially)
-  const [isFirstRun, setFirstRun] = useState(true); // on the first run, generates an empty 0x0 puzzle board, but is refreshed with a real puzzle after initialization
   const [getClickState, setClickState] = useState(0); // keep track of if the user is clicking or not; 0 is not clicking, 1 is clicking, 2 is right clicking
   const [isPuzzleSolved, setPuzzleSolved] = useState(false); // update if current puzzle is solved
   const [rowHints, setRowHints] = useState([]); // array representing the hints for each row from the JSON puzzle file; empty initially
   const [columnHints, setColumnHints] = useState([]); // array representing the hints for each column from the JSON puzzle file; empty initially
   const [rowHintsStatus, setRowHintsStatus] = useState([]); // array containing boolean values, changes to "false" if the row does not match the hint provided when the user checks their answer
   const [columnHintsStatus, setColumnHintsStatus] = useState([]); // array containing boolean values, changes to "false" if the column does not match the hint provided when the user checks their answer
-  
+  const [hasError, setHasError] = useState(false); // when user presses the "Check" button, if an error is found this boolean will change to true
+
   // Generate a new puzzle
   function getNewPuzzle(isFirstRun) {
     if (isFirstRun || isPuzzleSolved || confirm("Abandon the current puzzle?") == true) { //Don't show popup when generating the first puzzle or if the puzzle is solved
@@ -25,17 +25,22 @@ function Game(props) {
       setPuzzleNum(puzzleNum); //update puzzle
       setColumnHints([...puzzles[puzzleNum]["column"]]); //update hints
       setRowHints([...puzzles[puzzleNum]["row"]]); //update rows
-      setPuzzleSolved(false); // set puzzle to be unsolved if solved
+      setPuzzleSolved(false); // set puzzle to be unsolved
     }
   }
 
   //on initialization, generate a new random puzzle
   useEffect(() => {
-    if (isFirstRun) {
-      getNewPuzzle(true);
-    }
-    setFirstRun(false);
+    getNewPuzzle(true);
   }, []);
+
+  //when hints status array is updated after checking answers, add incorrect class to board to play shaking animation
+  useEffect(() => {
+    if(columnHintsStatus.includes(false) || rowHintsStatus.includes(false)){
+      setHasError(true);
+      setTimeout(()=> setHasError(false), 1500);
+    }
+  }, [columnHintsStatus, rowHintsStatus]);
 
   //Update the hints status arrays once the hints change (= when the puzzle changes)
   useEffect(() => {
@@ -65,7 +70,7 @@ function Game(props) {
 
   return (
     <>
-      <div className={styles.gameCnt} onContextMenu={handleContextMenu} onMouseUp={handleMouseUp} onTouchEnd={handleMouseUp}>
+      <div className={`${styles.gameCnt} ${hasError ? styles.hasError : ""}`} onContextMenu={handleContextMenu} onMouseUp={handleMouseUp} onTouchEnd={handleMouseUp}>
         <Hints rowHints={rowHints} columnHints={columnHints} rowHintsStatus={rowHintsStatus} columnHintsStatus={columnHintsStatus} />
         <GameBoard getPuzzleNum={getPuzzleNum} getNewPuzzle={getNewPuzzle} getClickState={getClickState} setClickState={setClickState} rowHints={rowHints} columnHints={columnHints} rowHintsStatus={rowHintsStatus} setRowHintsStatus={setRowHintsStatus} columnHintsStatus={columnHintsStatus} setColumnHintsStatus={setColumnHintsStatus} setPuzzleSolved={setPuzzleSolved} isPuzzleSolved={isPuzzleSolved} />
       </div>
